c2e69c800cd8c11cb714a70148eddcb6870c1524
Filip <56782972+FilipZobic@users.noreply.github.com>
2022-09-22 15:52:38 +0200

    [Feature] - Add continues integration (#1)
    * Add GitHub Actions gradle CI with cache
    
    * Add build scan URL as PR comment
    
    * Update to include comment on any failure
    
    * Add auto accept gradle build scan terms of service
    
    * Add temurine java 17 environment
    
    * Add success comment script on PR

--- src/test/java/io/zobic/gradlespringangular/integration/GradleSpringAngularApplicationTests.java
+++ src/test/java/io/zobic/gradlespringangular/integration/GradleSpringAngularApplicationTests.java
@@ -1,5 +1,6 @@
 package io.zobic.gradlespringangular.integration;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
