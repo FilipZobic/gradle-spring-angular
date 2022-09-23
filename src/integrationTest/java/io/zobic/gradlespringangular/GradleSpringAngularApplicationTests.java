3a27ec4f46972b6d80d5a6a3042c80555ac10b4c
Filip Zobic <zobicfilip919@gmail.com>
2022-09-23 16:54:21 +0200

    Update break task further & add gradle tree graph view plugin

--- src/integrationTest/java/io/zobic/gradlespringangular/GradleSpringAngularApplicationTests.java
+++ src/integrationTest/java/io/zobic/gradlespringangular/GradleSpringAngularApplicationTests.java
@@ -1,6 +1,5 @@
 package io.zobic.gradlespringangular;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +8,6 @@ class GradleSpringAngularApplicationTests {
 
 	@Test
 	void contextLoads() {
-
 	}
 
 }
--- src/test/java/io/zobic/gradlespringangular/UnitTestExampleTest.java
+++ src/test/java/io/zobic/gradlespringangular/UnitTestExampleTest.java
@@ -6,6 +6,5 @@ public class UnitTestExampleTest {
 
   @Test
   void hello() {
-
   }
 }
